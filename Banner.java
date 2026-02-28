0677845218d7477baaa706c3761fe0d9e720b894
Ulkeshtoke <Ulkeshtoke@gmail.com>
2026-02-28 22:16:27 +0530

    Connected Banner class with Main

--- Banner.java
+++ Banner.java
@@ -1,1 +1,6 @@
-// New class 
+public class Banner {
+
+    public void display() {
+        System.out.println("Welcome to OOPSBannerApp!");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-public class Main { public static void main(String[] args) { System.out.println("Welcome to OOPSBannerApp"); } } 
+public class Main {
+    public static void main(String[] args) {
+
+        Banner banner = new Banner();
+        banner.display();
+    }
+}
