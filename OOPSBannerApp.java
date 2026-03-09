cffc0bdc2e7287bbdb1cfb7ccf25183138010dc7
Ulkeshtoke <Ulkeshtoke@gmail.com>
2026-03-09 23:38:37 +0530

    Implemented UC7: CharacterPattern class for banner characters

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,67 +1,72 @@
-public class OOPSBannerApp {
-
-    public static String[] getO() {
-        return new String[] {
-            "    ***    ",
-            "  **   **  ",
-            " **     ** ",
-            " **     ** ",
-            " **     ** ",
-            " **     ** ",
-            " **     ** ",
-            "  **   **  ",
-            "    ***    "
-        };
-    }
-
-    public static String[] getP() {
-        return new String[] {
-            " *******    ",
-            " **     **  ",
-            " **      ** ",
-            " **     **  ",
-            " *******    ",
-            " **         ",
-            " **         ",
-            " **         ",
-            " **         "
-        };
-    }
-
-    public static String[] getS() {
-        return new String[] {
-            "    ***** ",
-            "  **      ",
-            "**        ",
-            " **       ",
-            "   ***    ",
-            "      **  ",
-            "       ** ",
-            "     **   ",
-            "*****     "
-        };
-    }
+public class OOPSBannerUC7 {
 
     public static void main(String[] args) {
 
-        String[] o = getO();
-        String[] p = getP();
-        String[] s = getS();
+        CharacterPattern o = new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
 
-        String[] bannerLines = new String[] {
-            String.join("", o[0], o[0], p[0], s[0]),
-            String.join("", o[1], o[1], p[1], s[1]),
-            String.join("", o[2], o[2], p[2], s[2]),
-            String.join("", o[3], o[3], p[3], s[3]),
-            String.join("", o[4], o[4], p[4], s[4]),
-            String.join("", o[5], o[5], p[5], s[5]),
-            String.join("", o[6], o[6], p[6], s[6]),
-            String.join("", o[7], o[7], p[7], s[7]),
-            String.join("", o[8], o[8], p[8], s[8])
-        };
+        CharacterPattern p = new CharacterPattern('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
 
-        for (String line : bannerLines) {
+        CharacterPattern s = new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern[] word = {o, o, p, s};
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            StringBuilder lineBuilder = new StringBuilder();
+
+            for (CharacterPattern cp : word) {
+                lineBuilder.append(cp.getPattern()[i]).append("   ");
+            }
+
+            banner[i] = lineBuilder.toString();
+        }
+
+        for (String line : banner) {
             System.out.println(line);
+        }
+    }
+
+    static class CharacterPattern {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
         }
     }
 }
