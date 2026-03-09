723171f03c0f45d5b557ca3f258112a6c6a38eba
Ulkeshtoke <Ulkeshtoke@gmail.com>
2026-03-09 23:18:06 +0530

    Implemented UC6: Static helper methods for banner characters

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,63 @@
 public class OOPSBannerApp {
 
+    public static String[] getO() {
+        return new String[] {
+            "    ***    ",
+            "  **   **  ",
+            " **     ** ",
+            " **     ** ",
+            " **     ** ",
+            " **     ** ",
+            " **     ** ",
+            "  **   **  ",
+            "    ***    "
+        };
+    }
+
+    public static String[] getP() {
+        return new String[] {
+            " *******    ",
+            " **     **  ",
+            " **      ** ",
+            " **     **  ",
+            " *******    ",
+            " **         ",
+            " **         ",
+            " **         ",
+            " **         "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[] {
+            "    ***** ",
+            "  **      ",
+            "**        ",
+            " **       ",
+            "   ***    ",
+            "      **  ",
+            "       ** ",
+            "     **   ",
+            "*****     "
+        };
+    }
+
     public static void main(String[] args) {
 
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
+
         String[] bannerLines = new String[] {
-            String.join(" ", "    *    ", "    *    ", " ***    ", "    *** "),
-            String.join(" ", "  *   *  ", "  *   *  ", " *     *  ", "  **      "),
-            String.join(" ", " *     * ", " *     * ", " *      * ", "**        "),
-            String.join(" ", " *     * ", " *     * ", " *     *  ", " **       "),
-            String.join(" ", " *     * ", " *     * ", " ***    ", "   *    "),
-            String.join(" ", " *     * ", " *     * ", " *         ", "      *  "),
-            String.join(" ", " *     * ", " *     * ", " *         ", "       * "),
-            String.join(" ", "  *   *  ", "  *   *  ", " *         ", "     *   "),
-            String.join(" ", "    *    ", "    *    ", " *         ", "**     ")
+            String.join("", o[0], o[0], p[0], s[0]),
+            String.join("", o[1], o[1], p[1], s[1]),
+            String.join("", o[2], o[2], p[2], s[2]),
+            String.join("", o[3], o[3], p[3], s[3]),
+            String.join("", o[4], o[4], p[4], s[4]),
+            String.join("", o[5], o[5], p[5], s[5]),
+            String.join("", o[6], o[6], p[6], s[6]),
+            String.join("", o[7], o[7], p[7], s[7]),
+            String.join("", o[8], o[8], p[8], s[8])
         };
 
         for (String line : bannerLines) {
